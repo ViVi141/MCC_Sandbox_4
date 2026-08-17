a4ba3c48bd0a17c6140d9529ff2b03b0ec8ebee6
ViVi141 <747384120@qq.com>
2026-08-17 20:54:39 +0800

    删除临时分析脚本

--- _show_mw.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys, io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
-p = r"addons\mcc_sandbox_mod\mcc\missionWizard\fnc\fn_MWSpawnInZone.sqf"
-with open(p, 'r', encoding='utf-8', errors='replace') as f:
-    lines = f.readlines()
-print("total:", len(lines))
-for i in range(97, 120):
-    print(i+1, repr(lines[i]))
--- _tail.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys, io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
-p = r"addons\mcc_sandbox_mod\mcc\dialogs\mcc_boxGen_change.sqf"
-with open(p, 'r', encoding='utf-8', errors='replace') as f:
-    lines = f.readlines()
-for i in range(214, min(225, len(lines))):
-    print(i+1, repr(lines[i]))
